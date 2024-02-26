1ca3ef3e6b94cfb3a8d81addd6e75baf3d3f5e8d
DiAnonim <akopyan.2002@mail.ru>
2024-02-26 21:07:13 +0600

    first commit

---
+++ django_hw/app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+import datetime
+
+# class Post(models.Model):
+#     title = models.CharField(max_length=100)
+#     content = models.TextField()
+#     published = models.DateField(default=datetime.date.today)
+
